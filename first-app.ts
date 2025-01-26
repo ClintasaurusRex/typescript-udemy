519c247fff5c97b1aa720eb8ac712452b5d3ff3b
Clint <clinthiles1992@gmail.com>
2025-01-26 15:12:17 -0800

    Migrate first-app.js to TypeScript with type annotations for variables

--- first-app.ts
+++ first-app.ts
@@ -1,1 +1,9 @@
-let userName = "Clint";
+let userName: string = "Clint";
+
+userName = "Clint";
+
+let userAge: number = 34;
+
+let isValid: boolean = true;
+
+// string, number, boolean the most important types
